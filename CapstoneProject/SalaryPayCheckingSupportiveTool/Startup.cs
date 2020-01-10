9da7b86804870112c6ce86b2e45083e63638bb7c
KhoiNKT <trongkhoi.trungmytayi@gmail.com>
2020-01-11 02:33:31 +0700

    Init project asp.net core with config for angular

---
+++ CapstoneProject/SalaryPayCheckingSupportiveTool/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalaryPayCheckingSupportiveTool
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // redirect non-API calls to angular to handle the routing
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                   !Path.HasExtension(context.Request.Path.Value) &&
+                   !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+            // config app for usage as API
+            app.UseMvcWithDefaultRoute();
+
+            // config app to serve the index.html file from /wwwroot when access the server from a browser
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
